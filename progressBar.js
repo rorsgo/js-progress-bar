0c8cc92e22eda6ceecc3ed3737aef7836a65a8e0
rorvts <rorvts@gmail.com>
2020-07-03 23:57:56 +0200

    Progress bar

--- progressBar.js
+++ progressBar.js
@@ -1,11 +1,21 @@
 var bar = document.createElement("div");
+var contentBox = document.querySelector(".content");
 
 bar.style.height = "10px";
-bar.style.width = "100%";
-bar.style.backgroundColor = "#9C9A9A";
+bar.style.width = "0px";
+bar.style.backgroundColor = "#3D3D3D";
 bar.style.position = "fixed";
 bar.style.top = "0";
 bar.style.left = "0";
 bar.style.zIndex = "9999";
+bar.style.transition = "0.2s";
 
 document.body.append(bar);
+
+document.addEventListener("scroll", () => {
+  const contentHeight = contentBox.offsetHeight;
+  const axisY = window.pageYOffset;
+
+  const updatedBar = axisY * 150 / contentHeight;
+  bar.style.width = updatedBar + "%";
+});
